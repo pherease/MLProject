ba8accc8fc09b7e744f3570d405d9314559b51bb
Elif Atılgan <efemyuksel@yandex.com>
2025-07-14 13:51:12 +0200

    Sync...

--- tiny.py
+++ tiny.py
@@ -10,7 +10,7 @@ import utils
 
 from sklearn.utils import class_weight
 from tensorflow.keras import layers, mixed_precision
-from utils import ConfusionMatrixSaver, csv_logger_cb, checkpoint_cb
+from utils import ConfusionMatrixSaver, csv_logger_cb, checkpoint_cb, make_augment
 
 
 # ───── reproducibility & logging ──────────────────────────────────
@@ -25,62 +25,55 @@ gpus = tf.config.list_physical_devices('GPU')
 if gpus:
     tf.config.experimental.set_memory_growth(gpus[0], True)
 
-DATA_CSV = "data.csv"
+TRAIN_CSV = "./train/train.csv"
+TEST_CSV = "./test/test.csv"
+VAL_CSV = "./validation/val.csv"
 # ───── Hyperparametes ─────────────────────────────────────────────────
 LEARN_RATE = 1e-4
 BATCH    = 2
 EPOCH = 60
-IMG_SIZE = 128
+IMG_SIZE = 256
 autotune = True
 
 # ───── load paths / labels & stratified split ────────────────────
-paths, labels, le = utils.load_paths_and_labels(DATA_CSV)
+train_paths, train_labels, train_label_encoder = utils.load_paths_and_labels(TRAIN_CSV)
+val_paths, val_labels, val_label_encoder = utils.load_paths_and_labels(VAL_CSV)
+test_paths, test_labels, test_label_encoder = utils.load_paths_and_labels(TEST_CSV)
 
-tr_p, va_p, te_p, tr_l, va_l, te_l = utils.stratified_split(
-    paths, labels, test_size=0.20, val_size=0.20, random_state=SEED
-)
+utils.show_dataset_class_distribution("TRAIN", train_labels)
+utils.show_dataset_class_distribution("VAL  ", val_labels)
+utils.show_dataset_class_distribution("TEST ", test_labels)
 
-utils.show_dataset_class_distribution("TRAIN", tr_l)
-utils.show_dataset_class_distribution("VAL  ", va_l)
-utils.show_dataset_class_distribution("TEST ", te_l)
-
-def make_augment(img_size = IMG_SIZE):
-    return tf.keras.Sequential([
-        layers.Resizing(img_size, img_size, name="resize"),
-        layers.RandomRotation(0.10),
-        layers.RandomFlip("horizontal_and_vertical"),
-        layers.RandomContrast(0.10),
-        layers.RandomTranslation(0.05, 0.05),
-        layers.Lambda(lambda t: tf.clip_by_value(t, 0., 1.))
-    ], name="augment")
-augment = make_augment(IMG_SIZE)
 # ───── tf.data pipelines identical to main.py ────────────────────
-train_ds = utils.make_dataset(tr_p, tr_l, BATCH, shuffle=True, autotune = autotune)
-train_ds = train_ds.map(lambda img, lbl: (augment(img), lbl), num_parallel_calls=tf.data.AUTOTUNE)
-val_ds   = utils.make_dataset(va_p, va_l, BATCH, shuffle=False, autotune = autotune)
-val_ds = val_ds.map(lambda img, lbl: (tf.image.resize(img, [IMG_SIZE, IMG_SIZE]), lbl), num_parallel_calls=tf.data.AUTOTUNE)
+augment_layer = make_augment()
+train_ds = utils.make_dataset(train_paths, train_labels, BATCH, shuffle=True, autotune = autotune)
+train_ds = train_ds.map(
+        lambda imgs, labs: (augment_layer(imgs, training=True), labs), num_parallel_calls=tf.data.AUTOTUNE).prefetch(tf.data.AUTOTUNE)
+val_ds   = utils.make_dataset(val_paths, val_labels, BATCH, shuffle=False, autotune = autotune)
+test_ds = utils.make_dataset(test_paths, test_labels, BATCH, shuffle=False, autotune = autotune)
 
 # ───── sanity-plot one val batch with augmentations ────────────────
-utils.show_image_of_batch(val_ds, le)
+utils.show_image_of_batch(train_ds, train_label_encoder)
 
 # ───── class weights to regularize differently sized categories ───────────
 cw_vals = class_weight.compute_class_weight(
     class_weight = 'balanced',
-    classes = np.unique(tr_l),
-    y = tr_l
+    classes = np.unique(train_labels),
+    y = train_labels
 )
 class_w = dict(enumerate(cw_vals))
 
 def build_model(num_classes: int = 9):
     model = tf.keras.Sequential([
     tf.keras.Input(shape=(IMG_SIZE, IMG_SIZE, 3)),
-    layers.Conv2D(32, (3,3), activation='relu', input_shape=(128,128,3)),
+    layers.Conv2D(32, (3,3), activation='relu'),
     layers.MaxPooling2D(2,2),
     layers.Conv2D(64, (3,3), activation='relu'),
     layers.MaxPooling2D(2,2),
     layers.Conv2D(128, (3,3), activation='relu'),
     layers.MaxPooling2D(2,2),
     layers.Flatten(),
+    layers.Dropout(0.5),
     layers.Dense(128, activation='relu'),
     layers.Dropout(0.5),
     layers.Dense(num_classes, activation='softmax')])
@@ -89,9 +82,7 @@ def build_model(num_classes: int = 9):
 
 loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
 
-
-
-model = build_model(num_classes=len(le.classes_))
+model = build_model(num_classes=len(train_label_encoder.classes_))
 model.compile(optimizer = tf.keras.optimizers.Adam(LEARN_RATE),
               loss = loss, 
               metrics=["accuracy"])
@@ -103,7 +94,7 @@ history = model.fit(
     validation_data=val_ds,
     epochs = EPOCH,
     class_weight=class_w,
-    callbacks=[ConfusionMatrixSaver(val_ds, le.classes_, every=3), checkpoint_cb(), csv_logger_cb()]
+    callbacks=[ConfusionMatrixSaver(val_ds, train_label_encoder.classes_, every=3), checkpoint_cb(), csv_logger_cb()]
 )
 model.save("model_trained.keras")
 
